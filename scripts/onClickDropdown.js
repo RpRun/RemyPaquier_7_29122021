6c2b052e496a19b96ed2f9f6a9f62527390a3dd9
RpRun <82530937+RpRun@users.noreply.github.com>
2021-12-31 14:41:10 +0100

    working on dropdown need past commits for css

--- scripts/onClickDropdown.js
+++ scripts/onClickDropdown.js
@@ -1,15 +1,62 @@
 const dropdowns = document.querySelectorAll('.dropdown')
+const dropdownBlocks = document.querySelectorAll('.dropdown-container')
 
 dropdowns.forEach(dropdown => {
+
+    const arrow = dropdown.querySelector('.arrow')
+    const list = dropdown.querySelector('.list')
+    const btnPlaceholder = dropdown.querySelector('.btn-placeholder')
+   
+
     dropdown.addEventListener('click', () => {
-        const arrow= dropdown.querySelector('.arrow')
-        const list = dropdown.querySelector('.list')
-        const btnPlaceholder = dropdown.querySelector('.btn-placeholder')
 
         btnPlaceholder.style.display = 'none'
         arrow.classList.add('reverse')
         list.classList.add('popIn')
 
         console.log('coucou');
+
     })
+
+// dropdownBlocks.forEach(dropdownBlock => {
+
+//     const arrow = dropdownBlock.querySelector('.arrow')
+//     const list = dropdownBlock.querySelector('.list')
+//     const btnPlaceholder = dropdownBlock.querySelector('.btn-placeholder')
+
+//     dropdownBlock.addEventListener('click', (event) => {
+//         if(event.target !== dropdownBlocks ) {
+
+//             btnPlaceholder.style.display = 'block'
+//             arrow.classList.remove('reverse')
+//             list.classList.remove('popIn')
+//             console.log('fuuuuck', event);
+//         }
+
+//     })
+// })
+
+
+  
 })
+
+
+
+
+
+
+
+// const dropdownBlocks = document.querySelectorAll('.dropdown-container')
+
+// dropdownBlocks.forEach(dropdownBlock => {
+//     dropdownBlock.addEventListener('click', (event) => {
+//         if(event.target !== dropdownBlocks) {
+
+//             btnPlaceholder.style.display = 'block'
+//             arrow.classList.remove('reverse')
+//             list.classList.remove('popIn')
+//             console.log('fuuuuck', event);
+//         }
+
+//     })
+// })
