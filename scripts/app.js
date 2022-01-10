5da00bc0f3d5f040462164ec94cb36301b9997f3
RpRun <82530937+RpRun@users.noreply.github.com>
2022-01-10 19:58:44 +0100

    cardsList on getData

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,22 @@
 import {
     recipes
 } from '../data/recipes.js'
-import { getData } from './getData.js'
-import { ingredientListHandler, onclickIngredientDropDown } from './onClickDropdown.js'
+
+import { createRecipeCard, getData } from './getData.js'
+import { ingredientListHandler, onclickIngredientDropDown } from './ingredientDropdownHandler.js'
 const DATA = recipes
 // console.log('all recipes', recipes)
 
 getData(DATA)
+
 onclickIngredientDropDown()
 ingredientListHandler(DATA)
+createRecipeCard(DATA)
+
+// const cardsList = document.querySelector(".thumbnails-list")
+// for(let i = 0; i < DATA.ingredients.length; i++) {
+//     const card = createRecipeCard(DATA.ingredients[i])
+//     cardsList.innerHTML += card
+//     console.log(cardsList)
+// }
 
--- scripts/getData.js
+++ scripts/getData.js
@@ -23,6 +23,8 @@ for (let i = 0; i < filteredIngredients.length; i++) {
     const li = document.createElement("li")
     li.innerHTML = ingredient
     list.append(li)
+    list.setAttribute(`tab-index`,0)
+    li.setAttribute(`tab-index`,0)  
 }
 const blocList = document.querySelector('.dropdown__list')
 
@@ -31,6 +33,43 @@ const createList = blocList.append(list)
 
 }
 
+export const createRecipeCard = (DATA) => {
+    
+const card = ` <li class="thumbnails__card">
+<img class="thumbnails__card--img" src="" alt="">
+<h2 class="thumbnails__heading">Limonade de Coco${DATA.ingredient} <span class="thumbnails__heading--timeToCook"> <img
+            src="./assets/icones/clock.png" alt="Temps de préparation">10 min${DATA.time}</span></h2>
+<div class="recipe">
+    <ul class="recipe__ingredients-list">
+        <li class="recipe-list__item">Lorem ipsum <span
+                class="recipe-list__item--quantity">400G</span></li>
+        <li class="recipe-list__item">sit amet consectetur <span
+                class="recipe-list__item--quantity">400G</span></li>
+        <li class="recipe-list__item">adipisicing elit. Ullam,?<span
+                class="recipe-list__item--quantity">400G</span></li>
+        <li class="recipe-list__item"> omnis commodi sunt maxime<span
+                class="recipe-list__item--quantity">400G</span></li>
+        <li class="recipe-list__item"> provident aspernatur<span
+                class="recipe-list__item--quantity">400G</span></li>
+    </ul>
+    <p class="recipe__instructions">Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+        pariatur molestias sapiente cupiditate consequatur consequuntur, temporibus quidem corporis
+        inventore nulla. Asperiores, eos voluptatem? Praesentium numquam recusandae voluptates
+        cupiditate placeat quidem! </p>
+</div>
+</li>`
 
+const cardsList = document.querySelector(".thumbnails-list")
+for (let i = 0; i < DATA.length; i++) {
+    const recipeCard = DATA[i];
+    recipeCard.innerHTML == card
+    cardsList.append(recipeCard)
+    // cardsList.innerHTML += card
+    console.log(card)
+}
+
+// const createCard = cardsList.append(recipeCard)
+
+}
 
 
