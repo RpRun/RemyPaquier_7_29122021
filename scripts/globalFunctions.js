59eeed776397bd9730ba66fb480605010dc62113
RpRun <82530937+RpRun@users.noreply.github.com>
2022-01-21 12:04:39 +0100

    working on search error message

--- scripts/globalFunctions.js
+++ scripts/globalFunctions.js
@@ -1,6 +1,6 @@
 const tagsList = document.querySelector('.tagsList')
 export const createTag = (content, type) => {
-    
+
     const div = document.createElement('div')
     div.classList.add('tag')
 
@@ -26,7 +26,7 @@ export const createTag = (content, type) => {
 const recipesList = document.querySelector('.thumbnails-list')
 
 export const displayRecipes = (DATA) => {
-   
+
     // on enlève toutes les recettes
     recipesList.innerHTML = ''
 
@@ -36,7 +36,7 @@ export const displayRecipes = (DATA) => {
         // Si il y a le paramètre display == true
         if (recipe.display) {
             let ingredientsList
-    
+
             for (let i = 0; i < recipe.ingredients.length; i++) {
                 const recipeIngredient = recipe.ingredients[i];
 
@@ -64,57 +64,159 @@ export const displayRecipes = (DATA) => {
                         <p class="recipe__instructions truncate-overflow">${recipe.description}</p>
                     </div>
                 </li>`
-    
-           
-                recipesList.innerHTML += card
-           
+
+
+            recipesList.innerHTML += card
+
         }
-        
+
     }
 }
 
 export const orderList = (element) => {
 
-    const orderedList = element.sort((a,b) => {
-        if(a.toLowerCase() < b.toLowerCase()) {
+    const orderedList = element.sort((a, b) => {
+        if (a.toLowerCase() < b.toLowerCase()) {
             return -1;
         }
-        if(a.toLowerCase() > b.toLowerCase()) {
+        if (a.toLowerCase() > b.toLowerCase()) {
             return 1;
         }
         return 0;
-        })
-        
-        return orderedList;
-    }
+    })
+
+    return orderedList;
+}
 
 const searchInput = document.querySelector("#search-bar-Field")
 
 
-searchInput.addEventListener ('input', (e) => {
+searchInput.addEventListener('input', (e) => {
     const searchedString = e.target.value.toLowerCase();
-
+    const searchResults = document.querySelectorAll(".thumbnails__card")
+    const searchResultList = document.querySelector(".thumbnails-list")
+    const main = document.querySelector('main')
+    const errorSearchMessage = document.querySelector('.error-message')
+    const visibleRecipes = document.querySelectorAll(".thumbnails__card:not(.thumbnails__card--hidden)")
    
-if (searchedString.length > 2 && recipesList.innerHTML.includes(searchedString)) {
+    // A partir de 3 lettres dans le champ de recherche, si la liste des recettes comporte les 3 lettres
+    if (searchedString.length >= 3 && recipesList.innerHTML.includes(searchedString)) {
+        console.log('on tape + de 3 lettres et il y a un resultat')
 
-    console.log('on est ds les conditions')
-    
-    const searchResult = document.querySelectorAll(".thumbnails__card")
-
-    for (let i = 0; i < searchResult.length; i++) {   
-    const searchResultItem = searchResult[i];
-    
-    searchResultItem.classList.add("thumbnails__card--hidden")
-   
-    if(searchResultItem.innerHTML.toLowerCase().includes(searchedString)){
-        searchResultItem.classList.remove("thumbnails__card--hidden")
+        // on cache tout le monde
+        searchResultList.classList.add("thumbnails__card--hidden")
+        console.log('on cache la liste')
+        // On fait reapparitre la liste des recettes
+        // On boucle sur la liste des recettes
+        for (let i = 0; i < searchResults.length; i++) {
+            const searchResultItem = searchResults[i];
+            searchResultItem.classList.add("thumbnails__card--hidden")
+            searchResultList.classList.remove("thumbnails__card--hidden")
+            console.log("on boucle, on cache toutes les cartes mais on fait reapparaitre la liste")
+            // Si une des recettes comporte la chaine de charactere renseignée dans le champ de recherche
+            if (searchResultItem.innerHTML.toLowerCase().includes(searchedString)) {
+                // on fait reapparaitre cette recette
+                console.log("on fait reapparaitre les resultats")
+                
+                searchResultItem.classList.remove("thumbnails__card--hidden") 
+            }
+        }       
+    }  
+    if(searchedString.length < 3/*en dessous de 3 caracteres*/) {
+        console.log("on affiche tout en dessous de 3 caracteres")
+        // réafficher toute la liste
+        searchResultList.classList.remove("thumbnails__card--hidden")
     }
-    console.log("on boucle")
     
-}
-
-   
     
-}
 
+    if(visibleRecipes.length > 0) {
+        // des recettes sont visibles, cacher le messsage d'erreur
+        errorSearchMessage.classList.add('error-message--hidden')
+        console.log(" des cartes ne sont pas cachees on cache le message d erreur")
+    } else {
+        // aucune recette visible, afficher le message d'erreur
+
+        errorSearchMessage.classList.remove('error-message--hidden')
+        console.log(errorSearchMessage)
+        console.log("on affiche le message car toutes les cartes sont cachees")
+    }
 })
+
+
+
+
+// searchInput.addEventListener('input', (e) => {
+//     const searchedString = e.target.value.toLowerCase();
+//     const searchResults = document.querySelectorAll(".thumbnails__card")
+//     const searchResultList = document.querySelector(".thumbnails-list")
+
+//     const errorSearchMessage = document.querySelector('.error-message')
+
+//     const main = document.querySelector('main')
+//     // Si on commence à renseigner le champs de recherche
+//     if (searchedString.length > 0 || searchedString.length < 3) {
+//         // On efface la liste des recettes
+//         searchResultList.classList.add("thumbnails__card--hidden")
+//     }
+//     if (searchedString.length >= 3 && searchResults.length < 1) {
+//         const errorSearchMessage = document.createElement('div')
+
+//         main.appendChild(errorSearchMessage)
+//         errorSearchMessage.classList.add('error-message')
+//         errorSearchMessage.innerHTML = "désoléééééé"
+//     }
+
+
+//     // A partir de 3 lettres dans le champ de recherche, si la liste des recettes comporte les 3 lettres
+//     if (searchedString.length >= 3 && recipesList.innerHTML.includes(searchedString)) {
+//         // On fait reapparitre la liste des recettes
+//         searchResultList.classList.remove("thumbnails__card--hidden")
+//         console.log('on est ds les conditions')
+//         // On boucle sur la liste des recettes
+//         for (let i = 0; i < searchResults.length; i++) {
+//             const searchResultItem = searchResults[i];
+//             // On fait disparaitre toutes les recettes
+//             searchResultItem.classList.add("thumbnails__card--hidden")
+//             // Si une des recettes comporte la chaine de charactere renseignée dans le champ de recherche
+//             if (searchResultItem.innerHTML.toLowerCase().includes(searchedString)) {
+//                 // on fait reapparaitre cette recette
+                
+//                 searchResultItem.classList.remove("thumbnails__card--hidden")
+//             }
+//             console.log("on boucle on trouve")
+//             const visibleRecipes = document.querySelector(".thumbnails__card:not(.thumbnails__card--hidden)")
+
+//             if(visibleRecipes.length > 0) {
+//                 // des recettes sont visibles, cacher le messsage d'erreur
+//                 errorSearchMessage.classList.add('error-message--hidden')
+//                 console.log(" des cartes ne sont pas cachees on cache le message d erreur")
+//             } else {
+//                 // aucune recette visible, afficher le message d'erreur
+
+//                 errorSearchMessage.classList.remove('error-message--hidden')
+//                 console.log(errorSearchMessage)
+//                 console.log("on affiche le message car toutes les cartes sont cachees")
+//             }
+//                 }
+
+//     }
+
+    
+// })
+
+
+
+ // tentative pour le message d erreur, je prefererais un autre if avec l inverse de includes>>>
+
+    //  else {
+    //     const errorSearchMessage = document.createElement('div')
+
+    //     main.appendChild(errorSearchMessage)
+    //     errorSearchMessage.classList.add('error-message')
+    //     errorSearchMessage.innerHTML = "désoléééééé"
+    //         }
+
+    //         if (searchedString.length <= 3) {
+    //             main.removeChild(errorSearchMessage)
+    //         }
